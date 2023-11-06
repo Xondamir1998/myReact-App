2b5c3c2d791238de50d2ff11c7d3843439bdfba0
Xondamir1998 <xondamirdekhkanov@mail.ru>
2023-11-06 20:23:00 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const Text = ({ todoNote }) => {
+  return (
+    <div className="text">
+      {todoNote}
+    </div>
+  );
+}
 
 function App() {
+  const [todoNote, setTodoNote] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+
+  const getUserValue = () => {
+    if (editingIndex !== null) {
+      const newTodoList = [...todoList];
+      newTodoList[editingIndex] = todoNote;
+      setTodoList(newTodoList);
+      setEditingIndex(null);
+    } else {
+      setTodoList([...todoList, todoNote]);
+    }
+    setTodoNote('');
+  }
+
+  const clearAll = () => {
+    setTodoList([]);
+    setEditingIndex(null);
+  }
+
+  const deleteCurrent = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+    setEditingIndex(null);
+  }
+
+  const editCurrent = (index) => {
+    setTodoNote(todoList[index]);
+    setEditingIndex(index);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <h1>Todo App</h1>
+        <div className="display">
+          <input
+            type="text"
+            id="user_text"
+            placeholder="Add your new todo"
+            value={todoNote}
+            onChange={(e) => setTodoNote(e.target.value)}
+          />
+          <button id="btn1" onClick={getUserValue}>
+            +
+          </button>
+        </div>
+
+        <div className="value_text">
+          {todoList.map((todo, index) => (
+            <div key={index} className="value_section">
+              <Text todoNote={todo} />
+              <i className="fa-solid fa-pen-to-square" onClick={() => editCurrent(index)}></i>
+              <i className="fa-solid fa-trash-can" onClick={() => deleteCurrent(index)}></i>
+            </div>
+          ))}
+        </div>
+
+        <div className="option">
+          <span>You have</span>&nbsp;<span className="n">{todoList.length}</span>&nbsp;
+          <span>pending tasks</span>
+          <button id="btn2" onClick={clearAll}>Clear all</button>
+        </div>
+      </div>
     </div>
   );
 }
